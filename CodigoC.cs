19973ecd025ac650ec2b4c1512a255de743cde2f
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-01-29 08:27:40 -0300

    Digitar um número que exiba a sua metade.

---
+++ CodigoC.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Insira numero: ");
+      
+      float n1 = Convert.ToSingle(Console.ReadLine());
+      float resultado = n1 / 2;
+      
+      Console.Write("A metade do numero " + n1);
+          Console.Write(" é " + resultado);
+            Console.ReadKey(true);
+    }
+    
+  }
+  
+}
