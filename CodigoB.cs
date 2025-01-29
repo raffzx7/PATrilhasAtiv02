f153913bbd659a8559ce0b792aa2232e8772492c
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-01-29 08:28:40 -0300

    Digitar um número que exiba o seu dobro.

--- CodigoB.cs
+++ CodigoB.cs
@@ -9,10 +9,10 @@ namespace MyApplication
       Console.Write("Insira numero: ");
       
       float n1 = Convert.ToSingle(Console.ReadLine());
-      float soma = n1 * 2;
+      float resultado = n1 * 2;
       
       Console.Write("O dobro do numero " + n1);
-          Console.Write(" é " + soma);
+          Console.Write(" é " + resultado);
             Console.ReadKey(true);
     }
     
